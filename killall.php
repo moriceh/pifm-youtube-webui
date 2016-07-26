db6f7b7f23a7dea1bf99b87b0c010a43e794433e
moriceh <hugo.morice0407@gmail.com>
2016-07-26 20:55:33 +0200

    Update killall.php

--- killall.php
+++ killall.php
@@ -9,5 +9,5 @@ header( "refresh:1;url=index.html" );
 <body background="http://www.musicalityacademy.com/wp-content/uploads/2014/01/music-notes-white-background-wallpaper.gif">
 <br><br><br><br><br><br>
 <h1 style="text-align: center;">
-	<span style="font-size:28px;"><span style="font-family:verdana,geneva,sans-serif;"><strong>La transmission est arrêtée.</strong></span></span></h1>
+	<span style="font-size:28px;"><span style="font-family:verdana,geneva,sans-serif;"><strong>Broadcast stopped.</strong></span></span></h1>
 </body>
