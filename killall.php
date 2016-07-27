3c7d3f4d68d481723ec30256efd97006bc71c565
moriceh <hugo.morice0407@gmail.com>
2016-07-27 21:15:51 +0200

    Add files via upload

---
+++ killall.php
@@ -0,0 +1,19 @@
+<?php
+exec('sudo killall fm_transmitter');
+exec('sudo killall fm_transmitter');
+exec('sudo killall fm_transmitter');
+header( "refresh:1;url=index.html" );
+?>
+
+<html>
+
+<head>
+<title>PiFM WebUI</title>
+</head>
+
+<body background="bg.gif">
+<br><br><br><br><br><br>
+<h1 style="text-align: center;">
+	<span style="font-size:28px;"><span style="font-family:verdana,geneva,sans-serif;"><strong>Broadcast stopped.</strong></span></span></h1>
+</body>
+</heml>
